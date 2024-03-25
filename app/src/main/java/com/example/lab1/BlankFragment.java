96cea30d4164fc3cbc345b35f85717b6a132fb4a
Dmitrys72 <dmitriysuslov01@gmail.com>
2024-03-25 18:01:01 +0600

    v1.2

--- app/src/main/java/com/example/lab1/BlankFragment.java
+++ app/src/main/java/com/example/lab1/BlankFragment.java
@@ -7,12 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BlankFragment extends Fragment {
+
+    private TextView textView;
+    private Button changeTextButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        textView = view.findViewById(R.id.textView);
+        changeTextButton = view.findViewById(R.id.button);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                textView.setText(getString(R.string.Text2));
+                Toast.makeText(getActivity(), getString(R.string.Text2), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
